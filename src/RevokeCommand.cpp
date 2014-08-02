0aa52112d85f4a1d0bb6a4ccf417e95eef109365
Donald Whyte <dwhyte@helios.(none)>
2014-08-02 14:14:37 +0100

    Implemented REVOKE command

--- src/RevokeCommand.cpp
+++ src/RevokeCommand.cpp
@@ -1,4 +1,5 @@
 #include "RevokeCommand.hpp"
+#include <sstream>
 
 namespace cms
 {
@@ -16,7 +17,30 @@ namespace cms
 
 	std::string RevokeCommand::execute(OrderManagerPtr orderManager)
 	{
-		return "TODO";
+		
+		try
+		{
+			// Find order that should be revoked and
+			// check dealer is same as one that posted it
+			const Order& order = orderManager->getOrder(orderToRevoke);
+			if (order.dealerID() == this->dealerID())
+			{
+				orderManager->revokeOrder(orderToRevoke);
+				std::stringstream stream;
+				stream << orderToRevoke << " HAS BEEN REVOKED";
+				return stream.str();
+			}
+			// Dealer who posted order is not the same as the one
+			// who wants to revoke it
+			else
+			{
+				return "UNAUTHORIZED";
+			}
+		}
+		catch (const OrderException& ex) // order w/ id could not be found
+		{
+			return "UNKNOWN_ORDER";	
+		}
 	}
 
 }
